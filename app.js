6992f7a6ae1a936d02de64fd1b012f92e0feda17
Abdulmajeed Alomari <abdulmajeedmmo@gmail.com>
2019-06-13 16:55:42 +0300

    chore: Define initial structure of the app

--- app.js
+++ app.js
@@ -1,6 +1,32 @@
 const express = require('express')
 const app = express()
+const mainDebugger = require('debug')('app:main')
 const port = process.env.port || 3000
 
+// ===============
+// Defining routes
+// const moviesRoute = require('./routes/movies')
+
+mainDebugger('Defined all routes.')
+
+// =================================
+// Setting up middlewares and config
+app.use(express.json())
+
+mainDebugger('Configured application.')
+
+// =============================
+// Development environment setup
+if(app.get('env') !== 'production') {
+  // Load morgan package to show requests on the console
+  const morgan = require('morgan')
+
+  // Use morgan package with 'tiny' setting, to show needed request headers.
+  app.use(morgan('tiny'))
+
+  mainDebugger('Finished development environment setup.')
+}
+
 app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+app.listen(port, () => mainDebugger(`Movlex API is listening on port ${port}!`))
